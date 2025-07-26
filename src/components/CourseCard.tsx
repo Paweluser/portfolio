3108eca6121a10838eeb31960e55b0615cae2068
Pawel <ppazyrski@wp.pl>
2025-07-26 20:02:56 +0200

    Add Timeline component

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -6,7 +6,7 @@ type CourseCardProps = {
 
 export default function CourseCard({ title }: CourseCardProps) {
   return (
-    <div className="border-[var(--accent-color)] border-2 rounded-md m-4 p-4">
+    <div className="md:hidden border-[var(--accent-color)] border-2 rounded-md m-4 p-4">
       <p className="text-xl text-[var(--second-color)] duration-300 transition-colors hover:text-[var(--accent-color)] text-start w-fit">
         {title}
       </p>
--- src/components/WorkingCard.tsx
+++ src/components/WorkingCard.tsx
@@ -5,7 +5,7 @@ export default function WorkingCard() {
     <div className="border-[var(--accent-color)] border-2 rounded-md m-4 p-4 flex flex-col justify-start items-center">
       <Icon
         icon="heroicons-outline:cog-8-tooth"
-        className="text-[var(--accent-color)] w-1/2 h-1/2 animate-wiggle"
+        className="text-[var(--accent-color)] w-16 h-16 animate-wiggle"
       />
       <p className='text-[var(--second-color)] text-xl'>Still working...</p>
     </div>
